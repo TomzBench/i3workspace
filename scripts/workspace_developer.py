1b6b0a05dee19cf810682e8cf28329fdfeb95b66
thomas <thomas.chiantia@gmail.com>
2016-08-17 23:40:22 -0400

    added some python script to start a open up a layout I sometimes use

--- scripts/workspace_developer.py
+++ scripts/workspace_developer.py
@@ -2,22 +2,33 @@
 
 import i3ipc
 import time
+import os
+from argparse import ArgumentParser
 
 i3 = i3ipc.Connection()
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('-d');
+    args = parser.parse_args()
+    path = args.d;
+    if path is None:
+        path = os.getcwd()
+    
+    #print("exec i3-sensible-terminal -cd " + path)
+
     do_cmd("split v")
-    do_cmd("exec i3-sensible-terminal")
+    do_cmd("exec i3-sensible-terminal -cd " + path)
     do_cmd("focus up")
     do_cmd("split v")
     do_cmd("layout tabbed")
-    do_cmd("exec i3-sensible-terminal")
-    do_cmd("exec i3-sensible-terminal")
+    do_cmd("exec i3-sensible-terminal -cd " + path)
+    do_cmd("exec i3-sensible-terminal -cd " + path)
     do_cmd("focus left")
     do_cmd("focus left")
 
 def do_cmd(command):
-    #python shit async
+    #I am ignorant on how to do python async, so here is some shit code
     i3.command(command)
     time.sleep(.1)
 
